f7489b92a225cca65ec48e90c73e16632a448823
ssundaran <sumesh.sundaran@eqs.com>
2024-11-03 07:07:55 +0530

    for each

--- myApp/src/routes/foreach/+page.svelte
+++ myApp/src/routes/foreach/+page.svelte
@@ -1,27 +1,44 @@
 <script lang="ts">
   let family = [
     {
-      name: 'Sumesh', age : 44, position: 'Elder Person'
+      name: 'Sumesh', age : 44, position: 'Elder Person', id : 1,
     },
     {
-      name: 'Sali', age : 37, position: 'House Wife'
+      name: 'Sali', age : 38, position: 'House Wife', id : 2,
     },
     {
-      name: 'Theertha', age : 14, position: 'Daughter'
+      name: 'Theertha', age : 14, position: 'Daughter', id : 3,
     },
     {
-      name: 'Tejas', age : 11, position: 'Son'
+      name: 'Tejas', age : 11, position: 'Son', id : 4,
     }
   ]
+  // function removeSumesh (){
+  //   family = family.filter ( member => member.id != 1)
+  // }
+  // const removeSumesh = () => {
+  //   family = family.filter (member => member.id != 1)
+  // }
+
+  // Inline Event Handlers
+const removeSumesh = (id: number) => {
+  {console.log(id)}
+  family = family.filter( (members) => members.id != id)
+}
 </script>
 
 <svelte:head>
   <title>SvelteKit demo page</title>
 </svelte:head>
 <div>
-  {#each family as family (family.age)}
-    <h3>{family.name}, Age is {family.age} and the person is {family.position}</h3> <br>
-
+ 
+  {#each family as members (members.id)}
+   <div>
+    <h3>{members.name}, Age is {members.age} and the person is {members.position}</h3> <br>
+    <button on:click={ () => removeSumesh(members.id) }>{members.id}</button> <br> <br> 
+   </div>
+   {:else}
+   {console.log('No data')}
   {/each}
 </div>
 <style>
