f290cd60d7315df4d65af440d7804e6af5cabc7c
ssundaran <sumesh.sundaran@eqs.com>
2024-11-08 10:25:11 +0530

    modal with events

--- myApp/src/components/Modal.svelte
+++ myApp/src/components/Modal.svelte
@@ -1,28 +1,55 @@
 <script>
-    export let message = 'I am from Modal'
-    export let isLoggedIn = true
-    let modal = true
+    export let message = 'Default value'
+    export let isLoggedIn = false
+    export let afterLoggin = true
+    let showModal = false
+    const showModalFn = () => {
+        isLoggedIn = !isLoggedIn
+    }
+    
 </script>
 
 
 {#if isLoggedIn}
-    <div class="modal" class:conditional-class={modal}>
+    <dialog class="modal" class:if-logged-in={afterLoggin} open={showModal}>
         <div class="modal-inner">
             <h2>{message}</h2>
         </div>
-    </div>
+        
+    </dialog>
+    
 {/if}
+<button on:click={showModalFn}>Show modal</button>
 
 <style>
+    button{
+        padding: 20px;
+        color: #fff;
+        position: relative;
+        z-index: 10;
+        background-color: red;
+    }
     .modal{
         background-color: rgba(0, 0, 0, 0.5);
         position: fixed;
-        margin:  0 auto;
-        width: 500px;
         color: #222;
+        margin: auto;
+        width: 100%;
+        height: 100%;
+        display: flex;
+    }
+    .if-logged-in{
+        background-color: cadetblue;
     }
     .modal-inner{
         background-color: #fff;
-        padding: 10px;
+        padding: 30px;
+        max-width: 600px;
+        color: #222;
+        margin: auto;
+        vertical-align: top;
+        & h2{
+            color: #222;
+        }
     }
 </style>
---
+++ myApp/src/routes/box-modal/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import Modal from "../../components/Modal.svelte";
+  let isLoggedIn = false;
+  let showModal = false
+
+  const show = () => {
+    isLoggedIn = !isLoggedIn
+    showModal = true
+  }
+</script>
+
+<svelte:head>
+  <title>SvelteKit demo page</title>
+</svelte:head>
+<!-- <button on:click={show}>Show Modal</button> -->
+<Modal 
+  message='I am from parent page'
+/>
+
+<style>
+  body{
+    background-color: white;
+  }
+  button{
+    position: relative;
+    display: inline-block;
+    padding: 10px;
+    margin: 0 auto;
+    width: 200px;
+    background-color: #222;
+    color: #fff;
+  }
+</style>
